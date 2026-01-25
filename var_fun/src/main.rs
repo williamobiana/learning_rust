1934d201d2917e1d25470965b08134d0395f3045
williamobiana <williamobiana@gmail.com>
2026-01-25 13:24:13 +0000

    functions

--- var_fun/src/main.rs
+++ var_fun/src/main.rs
@@ -1,8 +1,34 @@
-fn example(a: i32, b: i32) -> i32 {
-    a + b
-}
-
 fn main() {
-    let x =example(1, 2);
-    println!("The result is: {}", x);
+    let gifts = [
+        "And a partridge in a pear tree",
+        "two turtle doves",
+        "three French hens",
+        "four calling birds",
+        "five golden rings",
+        "six geese a-laying",
+        "seven swans a-swimming",
+        "eight maids a-milking",
+        "nine ladies dancing",
+        "ten lords a-leaping",
+        "eleven pipers piping",
+        "twelve drummers drumming"
+    ];
+    
+    let days = [
+        "first", "second", "third", "fourth", "fifth", "sixth",
+        "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"
+    ];
+    
+    for day in 0..12 {
+        println!("\nOn the {} day of Christmas,", days[day]);
+        println!("my true love sent to me");
+        
+        if day == 0 {
+            println!("A partridge in a pear tree");
+        } else {
+            for gift_num in (0..=day).rev() {
+                println!("{}", gifts[gift_num]);
+            }
+        }
+    }
 }
