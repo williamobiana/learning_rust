a8c1f91fd0aaed899808c6a69bcdf0a40c18af7f
williamobiana <williamobiana@gmail.com>
2026-01-25 12:26:13 +0000

    functions

--- var_fun/src/main.rs
+++ var_fun/src/main.rs
@@ -1,12 +1,8 @@
-
-fn calculate_stats() -> (f64, f64, f64) {
-    let min = 10.5;
-    let max = 99.9;
-    let avg = 55.2;
-    (min, max, avg)  // Return tuple
+fn example(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 fn main() {
-    let (_minimum, _maximum, _average) = calculate_stats();
-    println!("min: {}, max: {}, avg: {}", _minimum, _maximum, _average);
+    let x =example(1, 2);
+    println!("The result is: {}", x);
 }
